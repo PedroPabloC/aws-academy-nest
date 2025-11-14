3fd789987227470210991d5b252a7651660dae61
Pedro Cauich <A19216289@alumnos.uady.mx>
2025-11-13 18:07:46 -0600

    clean for build

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AlumnosModule } from './alumnos/alumnos.module';
 import { ProfesoresModule } from './profesores/profesores.module';
 
 @Module({
   imports: [AlumnosModule, ProfesoresModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
